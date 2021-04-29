87af34782942420293fefbb13bb0d7ba764899f8
Nicholas Khorasani <nckhoras@uwaterloo.ca>
2021-04-28 22:59:00 -0400

    Added 200 data point dataset pickle file and csv file

--- Models/large_data_scrape.py
+++ Models/large_data_scrape.py
@@ -13,16 +13,16 @@ import datetime
 def get_date_range_for_season(year):
     switcher = {
         2020: ["october-2019", "november", "december", "january", "february", "march", "july", "august", "september", "october-2020"],
-        2019: pd.date_range('2018-11-05', '2019-3-31'),
-        2018: pd.date_range('2017-11-05', '2018-3-31'),
-        2017: pd.date_range('2016-11-05', '2017-3-31'),
-        2016: pd.date_range('2015-11-05', '2016-3-31'),
-        2015: pd.date_range('2014-11-05', '2015-3-31'),
-        2014: pd.date_range('2013-11-01', '2014-3-31'),
-        2013: pd.date_range('2012-11-01', '2013-3-31'),
-        2012: pd.date_range('2012-1-01', '2012-3-31'),
-        2011: pd.date_range('2010-11-01', '2011-3-31'),
-        2010: pd.date_range('2009-11-01', '2010-3-31')
+        2019: pd.date_range('2018-11-08', '2019-3-31'),
+        2018: pd.date_range('2017-11-08', '2018-3-31'),
+        2017: pd.date_range('2016-11-08', '2017-3-31'),
+        2016: pd.date_range('2015-11-08', '2016-3-31'),
+        2015: pd.date_range('2014-11-08', '2015-3-31'),
+        2014: pd.date_range('2013-11-01', '2014-3-31'), # bad year no data on jan-31-2013 for some reason
+        2013: pd.date_range('2012-11-08', '2013-3-31'),
+        2012: pd.date_range('2012-1-01', '2012-3-31'), # bad year no data in november/december :(
+        2011: pd.date_range('2010-11-08', '2011-3-31'), # bad year
+        2010: pd.date_range('2009-11-08', '2010-3-31') # bad year 
     }
     return switcher.get(year, "out of range... range is 2010-2020")
 
@@ -63,7 +63,7 @@ def scrape_stat_data(url, daterange, column:int):
         try:
             teams_list, data_list = scrape_column(response, column)
         except ValueError as e:
-            print(f'Value Error for {date}')
+            print(f'Value Error for {date.date()} {url}')
             continue
         data_array.append([[date for i in range(30)], teams_list, data_list])
     
@@ -71,7 +71,7 @@ def scrape_stat_data(url, daterange, column:int):
 
 
 def send_scrape_threads(DHolder: DataHolder, years, columns: List[int]):
-    dateRanges = [get_date_range_for_season(year) for year in years]
+    dateRanges = [get_date_range_for_season(year) for year in years if year not in [2010, 2011, 2012, 2014]] # 2012 and 2014 are werid data years
     numDateRanges = len(dateRanges)
     for r in dateRanges:
         print(r)
@@ -91,12 +91,22 @@ def send_scrape_threads(DHolder: DataHolder, years, columns: List[int]):
                 print(f"val: {val[0]} \n")  
                 data_list += val
                 # print(val[0])
-            DHolder.stat_dict[f'{endpoint}-col={col}'] = data_list
+            key = get_column_name(endpoint, col)
+            DHolder.stat_dict[key] = data_list
             # DHolder.print()
             # print(f"this took {round(t1-t0,2)} seconds USING {numDateRanges} WORKERS!!!.")
         t1 = time.time()
         print(f"It took {round(t1-t0,2)} seconds to parse cols {columns} for {endpoint}.")
 
+def get_column_name(endpoint, column):
+    if column == 2:
+        return f'{endpoint}-SeasonSoFar'
+    elif column == 3:
+        return f'{endpoint}-Last3'
+    elif column == 5:
+        return f'{endpoint}-Home'
+    elif column == 6:
+        return f'{endpoint}-Away'
 
 def get_team_data(startYear: int, endYear: int, endpoints: List[str], columns:List[int]):
     DHolder = DataHolder(endpoints)
@@ -126,7 +136,31 @@ def get_team_data(startYear: int, endYear: int, endpoints: List[str], columns:Li
     return DF
 
 if __name__=='__main__':
-    endpoints = ['opponent-points-per-game', 'points-per-game','true-shooting-percentage', 'opponent-true-shooting-percentage','assists-per-game', 'assists-per-possession', 'assist--per--turnover-ratio', 'turnovers-per-possession','defensive-efficiency', 'opponent-effective-field-goal-pct', 'opponent-4th-quarter-points-per-game', 'offensive-efficiency', 'average-scoring-margin', 'opponent-defensive-rebounding-pct', 'opponent-offensive-rebounding-pct', 'defensive-rebounding-pct', 'offensive-rebounding-pct','win-pct-all-games']
+    endpoints = [
+                #  'effective-field-goal-pct', 'true-shooting-percentage', 'shooting-pct',
+                #  'opponent-effective-field-goal-pct', 'opponent-true-shooting-percentage', 'opponent-shooting-pct',
+
+                #  'free-throw-pct', 'free-throws-made-per-game', 'fta-per-fga', 'free-throw-rate',
+                #  'opponent-free-throw-pct', 'opponent-free-throws-made-per-game', 'opponent-fta-per-fga', 'opponent-free-throw-rate',
+
+                #  'assists-per-game', 'assists-per-possession', 'assists-per-fgm', 'assist--per--turnover-ratio',
+                #  'opponent-assists-per-game', 'opponent-assists-per-possession', 'opponent-assists-per-fgm', 'opponent-assist--per--turnover-ratio',
+
+                #  'turnovers-per-possession', 'turnover-pct',
+                #  'opponent-turnovers-per-possession', 'opponent-turnover-pct',
+
+                 'offensive-efficiency',
+                 'defensive-efficiency',
+
+                 'points-per-game', '3rd-quarter-points-per-game', '4th-quarter-points-per-game', 'average-scoring-margin',
+                 'opponent-points-per-game', 'opponent-3rd-quarter-points-per-game', 'opponent-4th-quarter-points-per-game', 'opponent-average-scoring-margin',
+
+                 'total-rebounds-per-game', 'offensive-rebounding-pct', 'defensive-rebounding-pct', 'total-rebounding-percentage',
+                 'opponent-total-rebounds-per-game', 'opponent-offensive-rebounding-pct', 'opponent-defensive-rebounding-pct', 'opponent-steals-per-game', 
+                 
+                 'win-pct-all-games', 'average-biggest-lead', 'fastbreak-efficiency',
+                 'opponent-win-pct-all-games', 'opponent-average-biggest-lead', 'opponent-fastbreak-efficiency'
+                ]
     print(f'len endpoints: {len(endpoints)}')
     t0 = time.time()
     # col = 2 means season stat, col = 3 means last 3 stats, cols = 4 means last 1 stats, col = 5 means home stats, col = 6 means away stats 2, 3, 5, 6
@@ -135,4 +169,4 @@ if __name__=='__main__':
     secs = t1-t0
     time_taken = str(datetime.timedelta(seconds=secs))
     print(f"The time for {len(endpoints)} endpoints was {time_taken}")
-    DF.to_pickle('data_df_2015-2020-18-endpoints-4-cols.pickle')
+    DF.to_pickle('data_df_2015-2020-second-24-endpoints-4-cols.pickle')
